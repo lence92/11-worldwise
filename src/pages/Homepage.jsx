c4cad8f20fd8f56237c47269303bf5e1fda9d9f7
Lenche Petkovska <lenche.petkovska.icloud.com>
2024-02-12 11:45:06 +0100

    OUR-9 first commit

---
+++ src/pages/Homepage.jsx
@@ -0,0 +1,15 @@
+import PageNav from "../components/PageNav";
+
+function Homepage() {
+  return (
+    <div>
+      <PageNav />
+      <h1>WoldWise</h1>
+
+      {/* <a href="/pricing">Pricing</a> */}
+      {/* <Link to="/pricing">Pricing</Link> */}
+    </div>
+  );
+}
+
+export default Homepage;
---
+++ src/pages/PageNotFound.jsx
@@ -0,0 +1,11 @@
+import PageNav from "../components/PageNav";
+
+function PageNotFound() {
+  return (
+    <div>
+      <PageNav /> Not found :(
+    </div>
+  );
+}
+
+export default PageNotFound;
